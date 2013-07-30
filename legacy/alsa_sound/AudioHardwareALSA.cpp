b4c3a21b0b48f8808a4510f8dcd530bfad45943c
Shreyas Nagasandra Chandrasekhar <snagas@codeaurora.org>
2013-07-30 12:15:33 +0530

    alsa_sound: Fix to Support in-call uplink recording
    Uplink recording during voice call fails as the case was not
     getting handled while opening the input stream.
     -Added support for Incall Uplink recording in openInputStream.
    
    GTalk call doesn't work if call is started during an existing
     recording session. All subsequent calls are failing after this
     scenario
     - Move the pcm device node open to read() call instead which will
       avoid opening the pcm device node of normal recording session
       and doesn't go into the error state. This will allow GTalk call
       to work and also all the subsequent calls to work
    
    CRs-Fixed: 517810
    Change-Id: I440ac4090b9f3e3a70b8acb4a818cfbf97e8c868
    
    Conflicts:
    	legacy/alsa_sound/AudioHardwareALSA.cpp

--- legacy/alsa_sound/AudioHardwareALSA.cpp
+++ legacy/alsa_sound/AudioHardwareALSA.cpp
@@ -1843,6 +1843,20 @@ WORKAROUND:
                                     sizeof(alsa_handle.useCase));
                         }
                     }
+                } else if (*channels & AUDIO_CHANNEL_IN_VOICE_UPLINK) {
+                    if (mFusion3Platform) {
+                        mALSADevice->setVocRecMode(INCALL_REC_MONO);
+                        strlcpy(alsa_handle.useCase, SND_USE_CASE_MOD_CAPTURE_VOICE,
+                                sizeof(alsa_handle.useCase));
+                    } else {
+                        if (*format == AUDIO_FORMAT_AMR_WB) {
+                            strlcpy(alsa_handle.useCase, SND_USE_CASE_MOD_CAPTURE_MUSIC_COMPRESSED,
+                                    sizeof(alsa_handle.useCase));
+                        } else {
+                            strlcpy(alsa_handle.useCase, SND_USE_CASE_MOD_CAPTURE_MUSIC,
+                                    sizeof(alsa_handle.useCase));
+                        }
+                    }
                 }
 #if defined(QCOM_FM_ENABLED) || defined(STE_FM)
             } else if((devices == AudioSystem::DEVICE_IN_FM_RX)) {
@@ -1943,36 +1957,14 @@ WORKAROUND:
             {
             route_devices = devices | mCurDevice;
             }
-            mALSADevice->route(&(*it), route_devices, mode());
         } else {
 #ifdef QCOM_USBAUDIO_ENABLED
             if(devices & AudioSystem::DEVICE_IN_ANLG_DOCK_HEADSET ||
                devices & AudioSystem::DEVICE_IN_PROXY) {
                 devices |= AudioSystem::DEVICE_IN_PROXY;
                 ALOGD("routing everything from proxy");
-            mALSADevice->route(&(*it), devices, mode());
-            } else
-#endif
-            {
-                mALSADevice->route(&(*it), devices, mode());
             }
-        }
-
-        if(!strcmp(it->useCase, SND_USE_CASE_VERB_HIFI_REC) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_HIFI_LOWLATENCY_REC) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_HIFI_REC_COMPRESSED) ||
-#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
-           !strcmp(it->useCase, SND_USE_CASE_VERB_FM_REC) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_FM_A2DP_REC) ||
 #endif
-           !strcmp(it->useCase, SND_USE_CASE_VERB_DL_REC) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_UL_DL_REC) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_CAPTURE_COMPRESSED_VOICE_DL) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_CAPTURE_COMPRESSED_VOICE_UL_DL) ||
-           !strcmp(it->useCase, SND_USE_CASE_VERB_INCALL_REC)) {
-            snd_use_case_set(mUcMgr, "_verb", it->useCase);
-        } else {
-            snd_use_case_set(mUcMgr, "_enamod", it->useCase);
         }
         if(sampleRate) {
             it->sampleRate = *sampleRate;
@@ -2005,18 +1997,12 @@ WORKAROUND:
             }
         }
 #endif
-        err = mALSADevice->open(&(*it));
         if (*format == AUDIO_FORMAT_AMR_WB) {
              ALOGV("### Setting bufsize to 61");
              it->bufferSize = 61;
         }
-        if (err) {
-           ALOGE("Error opening pcm input device");
-           mDeviceList.erase(it);
-        } else {
-           in = new AudioStreamInALSA(this, &(*it), acoustics);
-           err = in->set(format, channels, sampleRate, devices);
-        }
+        in = new AudioStreamInALSA(this, &(*it), acoustics);
+        err = in->set(format, channels, sampleRate, devices);
         if (status) *status = err;
         return in;
       }
