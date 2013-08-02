54d6a4fb6ab3ee8664853acea2078e534cac7b27
huangqiwu <huangqiwu@xiaomi.com>
2013-08-02 09:20:12 +0800

    bring back audiod
    Change-Id: I3beb1901571eb280f62c80d14bf381346d481d23

--- legacy/Android.mk
+++ legacy/Android.mk
@@ -11,6 +11,7 @@ ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
     include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
     include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
+    include $(AUDIO_HW_ROOT)/audiod/Android.mk
 endif
 endif
 endif
